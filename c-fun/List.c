cd1a65e9717ee2a5daab0b8b6e8ef5b846487fd2
mohit <mohit.rathee2505@gmail.com>
2023-12-09 16:47:01 +0530

    append and pop

--- c-fun/List.c
+++ c-fun/List.c
@@ -1,10 +1,10 @@
+#include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
 typedef enum {
     Int,
     String,
-    List,
     Bool,
 } class ;
 
@@ -17,12 +17,12 @@ typedef union {
 typedef struct{
     class class;
     value data;
-} datatype;
+} dataObj;
 
 typedef struct {
     int capacity;
     int size;
-    datatype* arr;
+    dataObj* arr;
 } list ;
 
 list* initList(){ // I will give [1,2,3,4,] will create a list in future.
@@ -30,22 +30,22 @@ list* initList(){ // I will give [1,2,3,4,] will create a list in future.
     list *my_list = malloc(sizeof(list));
     my_list->capacity = capacity;
     my_list->size = 0;
-    my_list->arr = (datatype *) malloc(capacity*sizeof(datatype)) ; 
+    my_list->arr = (dataObj *) malloc(capacity*sizeof(dataObj)) ; 
     //filing data
-    my_list->arr[0]=(datatype){.class=Int,.data={.num=5}};
-    my_list->arr[1]=(datatype){.class=String,.data={.str="AAAA"}};
-    my_list->arr[2]=(datatype){.class=Bool,.data={.boool=true}};
-    my_list->size=4; //if error the put this to 3
+    my_list->arr[0]=(dataObj){.class=Int,.data={.num=5}};
+    my_list->arr[1]=(dataObj){.class=String,.data={.str="AAAA"}};
+    my_list->arr[2]=(dataObj){.class=Bool,.data={.boool=true}};
+    my_list->size=3; 
     return my_list;
 }
 
-void printList(list *my_list){
+void print(list *my_list){
     int capacity = my_list->capacity;
     int size = my_list->size;
     printf("capacity: %d \n",capacity);
     printf("size: %d \n",size);
     printf("[");
-    datatype* arr = my_list->arr;
+    dataObj* arr = my_list->arr;
     for (int i=0;i<size;i++){
         switch (arr[i].class) {
             case Int:
@@ -66,7 +66,36 @@ void printList(list *my_list){
     
 
 }
+void append(list* my_list, dataObj dataobj){
+    printf("type: %d\n",dataobj.class);
+    printf("%s\n",dataobj.data.str);
+    int capacity = my_list->capacity;
+    int size = my_list->size;
+    if (size>capacity){
+        //realloc
+    }
+    my_list->arr[size]= dataobj;
+    my_list->size++;
+}
+dataObj pop(list* my_list){
+    int size = my_list->size;
+    if (size<0){return (dataObj){};}
+    dataObj retVal = my_list->arr[size-1];
+    my_list->size--;
+    return retVal;
+}
 int main(){
-    list *myList = initList();
-    printList(myList);
+    list* myList = initList();
+    print(myList);
+
+    //Append
+    //Don't mismatch enumType and valueType
+    dataObj val = {.class=String,.data={.str="BBBB"}}; 
+    append(myList, val); 
+    print(myList);
+
+    //Pop
+    dataObj waste= pop(myList);
+    print(myList);
+    printf("%s\n",waste.data.str);
 }
