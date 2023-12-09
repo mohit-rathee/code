615dd64532b9840a2e2c7305d47abe2fd9cfec81
mohit <mohit.rathee2505@gmail.com>
2023-12-09 16:52:30 +0530

    insert is bad

--- python/pre.py
+++ python/pre.py
@@ -1,6 +1,6 @@
 import random
-a=[0]*100000000 
-for i in range(100000000):
+a=[0]*100000
+for i in range(99999,0,-1):
     s = random.randint(0,100)
     a[i]=s
     b={}
--- python/push.py
+++ python/push.py
@@ -1,8 +1,8 @@
 import random
 a=[]
-for i in range(100000000):
+for i in range(100000):
     s = random.randint(0,100)
-    a.append(s)
+    a.insert(0,s)
     b={}
     for j in range(10):
         b[j]=s
