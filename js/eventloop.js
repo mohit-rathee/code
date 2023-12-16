3af923519761bbab3ad9fb504c40c0f0f4c94642
mohit <mohit.rathee2505@gmail.com>
2023-12-16 20:39:36 +0530

    eventloop

---
+++ js/eventloop.js
@@ -0,0 +1,21 @@
+setTimeout(
+    ()=>{console.log('time outs !!!')},
+    0
+);
+async function important(){
+    fetch("https://google.com")
+    .then((txt)=>{
+            console.log('response arrived');
+        })
+}
+important();
+
+
+console.log('starting');
+
+for(let i=0;i<5000000000;i++){
+    //do something to aquire main thread.
+    let some = i+i-i;
+}
+
+console.log('long long time later!!!');
