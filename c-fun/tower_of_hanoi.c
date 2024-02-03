35c0f4d81a5e8204ea26b5bb6a2fa3593782c81e
mohit <mohit.rathee2505@gmail.com>
2024-02-03 23:04:40 +0530

    tower_of_hanoi understood

--- c-fun/tower_of_hanoi.c
+++ c-fun/tower_of_hanoi.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 void tower_of_hanoi(int n,char from,char to,char alt){
-    for (int i=0;i<(3-n);i++) {
-        printf("    |");
-    }
-    printf("n: %d,[ %c ->  %c (%c) ]\n",n,from,to,alt);
+
     if (n==0) {
         return;
     }
+    // Youtube Video:
+    //      https://www.youtube.com/watch?v=2SUvWfNJSsM
+
+    // move (n-1)th disk from top of me to alt(not dest)
+    // alt and to are replaced because n don't wants (n-1)
+    // to be at n's destination
     tower_of_hanoi(n-1,from, alt, to);
 
-    for (int i=0;i<(3-n);i++) {
-        printf("    |");
-    }
-    printf(" (%d) %c ==> %c\n",n,from,to);
+    // move me to the destination.
+    printf("(%d) %c ==> %c\n",n,from,to);
 
+    // move (n-1)th disk from alt to top of  me 
     tower_of_hanoi(n-1, alt, to, from);
     return;
 }
