71863f88ca33932629a208c5a718131930edad2b
mohit <mohit.rathee2505@gmail.com>
2024-02-05 00:00:25 +0530

    node_at_distance

---
+++ gfg/node_at_distance.py
@@ -0,0 +1,21 @@
+def count_k_dist_nodes(root, k, level, map, unique_nodes):
+    if not root:
+        return
+    level += 1
+    map[level] = root
+    if root.left is None and root.right is None:
+        if level >= k+1:
+            unique_nodes.add(map.get(level-k))
+    count_k_dist_nodes(root.right, k, level, map, unique_nodes)
+    count_k_dist_nodes(root.left, k, level, map, unique_nodes)
+
+def printKDistantfromLeaf(root, k):
+    if not root:
+        return 0
+    unique_nodes = set()
+    map = {}
+    level = 0
+    count_k_dist_nodes(root, k, level, map, unique_nodes)
+    return len(unique_nodes)    
+
+
