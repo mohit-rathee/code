ab1feeaa8826c3dc3086d692bd2519cd72c3cd70
mohit <mohit.rathee2505@gmail.com>
2024-02-10 23:09:15 +0530

    timeLimitExeed

---
+++ gfg/no_of_path_in_matrix.py
@@ -0,0 +1,23 @@
+def numberOfPath (n, k, arr):
+    counter=[0]
+    def recurse(sum,x,y):
+        sum+=arr[x][y]
+        if x+y+2==2*n:
+            if sum==k:
+                counter[0]+=1
+            return
+        if x<n-1:
+            recurse(sum,x+1,y)
+        if y<n-1:
+            recurse(sum,x,y+1)
+
+    recurse(0,0,0)
+    print(counter[0])
+
+Arr = [
+    [1,2,3],
+    [2,3,4],
+    [3,4,5],
+]
+print(Arr)
+numberOfPath(3,15,Arr)
