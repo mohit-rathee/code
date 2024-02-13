ce0e41073093de075a6e6d2f4cd374d3fac66880
mohit <mohit.rathee2505@gmail.com>
2024-02-13 22:46:49 +0530

    bbst start

--- rust/bbst/src/main.rs
+++ rust/bbst/src/main.rs
@@ -1,3 +1,22 @@
+#[derive(Debug)]
+struct Node{
+    val  : usize,
+    left : Option<Box<Node>>,
+    right: Option<Box<Node>>,
+}
+impl Node {
+    fn new(val:usize) -> Node { Node{val,left:None,right:None} }
+}
+
+struct BBST {
+    root: Box<Node>,
+}
+impl BBST {
+    fn new(val:usize) -> BBST {
+        BBST{ root : Box::new(Node::new(val)) }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let my_bbst = BBST::new(50);
+    println!("{}",my_bbst.root.val);
 }
