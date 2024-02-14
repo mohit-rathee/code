bdf7a14955addc323a5bf9402dcaaa9d69560c13
mohit <mohit.rathee2505@gmail.com>
2024-02-14 20:38:42 +0530

    c-trick

--- c-fun/arrays.c
+++ c-fun/arrays.c
@@ -1,10 +1,14 @@
+#include <stdio.h>
 int  main(){
-    int arr[5];
-    int i = 0;
-    while(i!=5){
-        int a = i;
-        //scanf("%d",&a);
-        arr[i]=a;
-        i++;
-    }
+    int arr[] = {1,2,3,4,5};
+    printf("%p\n",arr);
+
+    // ( A[B] || B[A] ) ==> A+B*sizeof(el)
+    //                      where :
+    //                          A = pointer
+    //                          B = int
+    printf("%d\n",arr[0]);
+    printf("%d\n",0[arr]);
+
+    printf("%d\n",*arr);
 }
