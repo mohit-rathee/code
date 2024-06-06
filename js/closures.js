f4c5c56fe4d129ebbd7c0b6af1ed842608c2c6f2
mohit <mohit.rathee2505@gmail.com>
2024-06-06 13:15:33 +0530

    js

--- js/closures.js
+++ js/closures.js
@@ -1,33 +1,32 @@
 // I m learning from namaste javascript
 
-function a(){
-    for (var i=0;i<6;i++){
-        setTimeout(function (){
-            if(i==6) i=1;
-            console.log(i);
-            i++;
-        },i*1000); 
-    }
+function a() {
+  for (let i = 0; i < 6; i++) {
+    setTimeout(function () {
+      if (i == 6) i = 1;
+      console.log(i);
+      i++;
+    }, i * 1000);
+  }
 }
 
-function b(){
-    for(var i=0;i<6;i++){
-        function magic(x){
-            setTimeout(function(){
-                console.log(x);
-            },x*1000)
-
-            }
-        magic(i);
-    }
+function magic(x) {
+  setTimeout(function () {
+    console.log(x);
+  }, x * 1000);
+}
+function b() {
+  for (let i = 0; i < 6; i++) {
+    magic(i);
+  }
 }
 
-function c(){
-    for (let i=0;i<6;i++){
-        setTimeout(function (){
-            console.log(i);
-        },i*1000); 
-    }
+function c() {
+  for (let i = 0; i < 6; i++) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  }
 }
 
 a();
