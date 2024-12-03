36ef198d018d45afbaec6eba1aa3a97d83ef5bc8
mohit <mohit.rathee2505@gmail.com>
2024-12-03 17:27:14 +0530

    Advent Of Code 2024

---
+++ advent2024/run.py
@@ -0,0 +1,32 @@
+import sys
+import importlib
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python run.py <day> <part>")
+        print("Example: python run.py day1 part1")
+        sys.exit(1)
+
+    day = sys.argv[1]
+    part = sys.argv[2]
+
+    # Construct the module path
+    module_path = f"solutions.{day}.{part}"
+
+    try:
+        # Dynamically import the module
+        module = importlib.import_module(module_path)
+
+        # Check if the module has a main function
+        if hasattr(module, "main"):
+            print(f"Running {day}/{part}...")
+            module.main()  # Call the main function
+        else:
+            print(f"Error: The module '{module_path}' does not have a 'main()' function.")
+    except ModuleNotFoundError:
+        print(f"Error: Module '{module_path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
---
+++ advent2024/utils/inputReader.py
@@ -0,0 +1,7 @@
+def parseSpaceSeparatedLists(inputFile):
+    with open("inputs/" + inputFile, "r") as f:
+        data = [line.strip().split('   ') for line in f.readlines()]
+        firstList = [el[0] for el in data]
+        secondList = [el[1] for el in data]
+    return [firstList, secondList]
+
