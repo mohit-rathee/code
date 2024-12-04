ac39d0249c670c6c7197a07e55cc27566b8a7a62
mohit <mohit.rathee2505@gmail.com>
2024-12-04 10:58:38 +0530

    day2 part1

--- advent2024/utils/inputReader.py
+++ advent2024/utils/inputReader.py
@@ -1,7 +1,11 @@
-def parseSpaceSeparatedLists(inputFile):
+def parse2SpaceSeparatedLists(inputFile):
     with open("inputs/" + inputFile, "r") as f:
         data = [line.strip().split('   ') for line in f.readlines()]
         firstList = [el[0] for el in data]
         secondList = [el[1] for el in data]
     return [firstList, secondList]
 
+def parseLevels(inputFile):
+    with open("inputs/" + inputFile, "r") as f:
+        data = [line.strip().split(' ') for line in f.readlines()]
+    return data
