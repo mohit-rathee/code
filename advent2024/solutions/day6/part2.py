29021d3354615ad747105cddb520bdd3034cec2f
mohit <mohit.rathee2505@gmail.com>
2024-12-07 21:35:50 +0530

    another failed attempt

--- advent2024/solutions/day6/part2.py
+++ advent2024/solutions/day6/part2.py
@@ -6,107 +6,120 @@ from utils.inputReader import parseGrid
 # Y-1 ===> GOING LEFT
 
 directionVectors = {
-    'top': (-1, 0),
-    'bottom': (1, 0),
-    'left': (0, -1),
-    'right': (0, 1),
+    't': (-1, 0),
+    'b': (1, 0),
+    'l': (0, -1),
+    'r': (0, 1),
 }
+pattern = "*"
 
 
-def printGrid(grid):
-    for x in grid:
-        for y in x:
-            print(y, end='\t')
-        print()
+def rotate(dir):
+    if dir == 't':
+        dir = 'r'
+    elif dir == 'r':
+        dir = 'b'
+    elif dir == 'b':
+        dir = 'l'
+    elif dir == 'l':
+        dir = 't'
+    return dir
 
 
-def main():
-    pattern = 1
-    grid = parseGrid('problem6')
+def traverse(grid):
     # printGrid(grid)
     maxX = len(grid)
     maxY = len(grid[0])
     # find ^
     X = Y = 0
-    dir = 'top'
+    dir = 't'
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == '^':
                 X = x
                 Y = y
-    # map a square from the last 3 collision points and place obstruction at the
-    # 4th corner + directionVector, if there is no obstruction btw 3rd and 4th one.
-    collisions = []
-    square_loops = []
-    printGrid(grid)
+    # printGrid(grid)
     while X > -1 and X < maxX and Y > -1 and Y < maxY:
+        # print()
         if (grid[X][Y] == "#"):
+            # print(dir)
+            # print(X, Y)
+            # print('going back')
             X = X-directionVectors[dir][0]
             Y = Y-directionVectors[dir][1]
-            if (len(collisions) >= 3):
-                last_collision = collisions[-1]
-                fourth_corner = map_square(collisions[-3:])
-                current_pos = [X, Y]
-                if not checkCollinearInbound(
-                    last_collision, fourth_corner, current_pos
-                ):
-                    square_loops.append(fourth_corner)
-                collisions.append([X, Y])
+            dir = rotate(dir)
+        else:
+            if type(grid[X][Y]) == list:
+                grid[X][Y].append(dir)
             else:
-                collisions.append([X, Y])
-            # if grid[X][Y] == "*":
-            #     grid[X][Y] = '@'
-            # else:
-            #     grid[X][Y] = "*"
-            if dir == 'top':
-                dir = 'right'
-            elif dir == 'right':
-                dir = 'bottom'
-            elif dir == 'bottom':
-                dir = 'left'
-            elif dir == 'left':
-                dir = 'top'
-        # else:
-        #     grid[X][Y] = '*'
+                grid[X][Y] = [dir]
         # print(dir)
         # print(X, Y)
         X = X+directionVectors[dir][0]
         Y = Y+directionVectors[dir][1]
-        # print()
-        # print()
-        # print()
-        # print()
+    return grid
+
+
+def printGrid(grid):
+    for x in grid:
+        for y in x:
+            if type(y) == list:
+                for i in y:
+                    print(i, end='')
+                print('\t', end='')
+            else:
+                print(y, end='\t')
+        print()
+
+
+def main():
+    grid = parseGrid('problem6')
+    grid = traverse(grid)
+    loops = check_all_obstruction_points(grid)
+    print(loops)
+    printGrid(grid)
+
+
+def check_all_obstruction_points(grid):
+    points = set()
+    maxX = len(grid)
+    maxY = len(grid[0])
     # printGrid(grid)
-    print(square_loops)
-    print(len(square_loops))
-    # print(calculate_path_length(grid, pattern))
+    for x in range(2, maxX):
+        for y in range(maxY):
+            if type(grid[x][y]) == list:
+                # print(grid[x][y])
+                pos = check_this_obstruction_point(grid, (x, y))
+                if (pos):
+                    points.add(pos)
+    # pos = check_this_obstruction_point(grid, (6, 4))
+    return points
 
 
-def map_square(last3colissions):
-    [a, b, c] = last3colissions
-    x_gap = b[0] - a[0]
-    y_gap = b[1] - a[1]
-    fourth_corner = [c[0] - x_gap, c[1] - y_gap]
-    # print('4th corner', fourth_corner)
-    return fourth_corner
+def check_this_obstruction_point(grid, pos):
+    maxX = len(grid)
+    maxY = len(grid[0])
+    # print(pos, end=' ')
+    (x, y) = pos
+    dir = grid[x][y]
+    for d in dir:
+        new_d = rotate(d)
+        if new_d in dir:
+            return False
+        # print(new_d)
+        while x in range(maxX) and y in range(maxY) and grid[x][y] != "#":
+            if type(grid[x][y]) == list:
+                if new_d in grid[x][y]:
+                    (x, y) = pos
+                    x += directionVectors[d][0]
+                    y += directionVectors[d][1]
+                    if (grid[x][y] != "#"):
+                        # print('found for ', pos, d, (x, y))
+                        return (x, y)
+                    # print("# already obstructed")
+                    return False
+            x += directionVectors[new_d][0]
+            y += directionVectors[new_d][1]
+        # print('not found')
+        return False
 
-
-def checkCollinearInbound(p1, p2, pos):
-    # print(p1,p2,pos)
-    [x1, y1] = p1
-    [x2, y2] = p2
-    [x, y] = pos
-    if x1 == x2:  # Vertical line
-        return x == x1 and min(y1, y2) <= y <= max(y1, y2)
-
-    elif y1 == y2:  # Horizontal line
-        return y == y1 and min(x1, x2) <= x <= max(x1, x2)
-
-
-def calculate_path_length(grid, pattern):
-    counter = 0
-    for i in grid:
-        for j in i:
-            if j == pattern:
-                counter += 1
-    return counter
