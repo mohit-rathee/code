c35d68654675dbdc31c36a9335c4f0ba9038568a
mohit <mohit.rathee2505@gmail.com>
2024-12-08 11:31:20 +0530

    day7 part1
    bruteforce all possible ways of calculating the target value

--- advent2024/utils/inputReader.py
+++ advent2024/utils/inputReader.py
@@ -5,15 +5,18 @@ def parse2SpaceSeparatedLists(inputFile):
         secondList = [el[1] for el in data]
     return [firstList, secondList]
 
+
 def parseLevels(inputFile):
     with open("inputs/" + inputFile, "r") as f:
         data = [line.strip().split(' ') for line in f.readlines()]
     return data
 
+
 def parseGrid(inputFile):
     with open("inputs/" + inputFile, "r") as f:
         data = [list(line.strip()) for line in f.readlines()]
     return data
+
 
 def day5Input(inputFile):
     with open("inputs/" + inputFile, "r") as f:
@@ -23,10 +26,19 @@ def day5Input(inputFile):
         idx = 0
         while data[idx]:
             priorityQ.append(data[idx].split('|'))
-            idx+=1
-        idx+=1
-        while idx<len(data):
+            idx += 1
+        idx += 1
+        while idx < len(data):
             printQ.append(data[idx].split(','))
-            idx+=1
+            idx += 1
 
-        return [priorityQ,printQ]
+        return [priorityQ, printQ]
+
+
+def day7Input(inputFile):
+    with open("inputs/" + inputFile, "r") as f:
+        data = [line.strip().split(':') for line in f.readlines()]
+        result = []
+        for i in data:
+            result.append([i[0],i[1].strip().split(' ')]) 
+        return result
