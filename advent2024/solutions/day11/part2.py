1a0acbd2f5f97f33e28273a10775812e2450c70a
mohit <mohit.rathee2505@gmail.com>
2024-12-11 12:14:12 +0530

    with dp + recursive + without simulating actual array

--- advent2024/solutions/day11/part2.py
+++ advent2024/solutions/day11/part2.py
@@ -1,44 +1,42 @@
 from utils.inputReader import parseLevels
+import sys
+sys.setrecursionlimit(100**3)
 
 
 dp = {}
+""" We will store pairs of (num,level) in dp """
 
 
-def solve(num):
-    if num in dp:
-        return dp[num]
+def solve(num, lvl):
+    if (num, lvl) in dp:
+        # print('found for',(num,lvl),':',dp[(num,lvl)])
+        return dp[(num, lvl)]
+    if lvl == 75:
+        return 1  # length
+    if num == 0:
+        ret = solve(1, lvl+1)
     elif len(str(num)) % 2 == 0:
         str_stone = str(num)
         half = len(str(num))//2
-        left_stone = int(str_stone[:half])
-        right_stone = int(str_stone[half:])
-        ret = [left_stone, right_stone]
+        left = int(str_stone[:half])
+        right = int(str_stone[half:])
+        ret = solve(left, lvl+1)+solve(right, lvl+1)
     else:
-        new_num = num*2024
-        ret = [new_num]
-    dp[num] = ret
+        ret = solve(num*2024, lvl+1)
+    dp[(num, lvl)] = ret
     return ret
 
 
 def main():
     data = parseLevels('problem11')
     stones = data[0]
-    for count in range(75):
-        next_arrangment = []
-        for stone in stones:
-            # print('for ',stone)
-            stone = int(stone)
-            if stone==0:
-                next_arrangment.append(1)
-                continue
-            new_stones = solve(stone)
-            # print('new_stones',new_stones)
-            for s in new_stones:
-                next_arrangment.append(s)
-            stones = next_arrangment
-            # print(stones)
-        print(count,len(stones))
-        # for i in stones:
-        #     print(i, end=' ')
-        # print()
-    print(len(stones))
+    print(stones)
+    ans = 0
+    for stone in stones:
+        stone = int(stone)
+        length = solve(stone, 0)
+        ans += length
+
+    # for k,v in dp.items():
+    #     print(k,":",v)
+    print(ans)
