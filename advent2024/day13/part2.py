41f12db5f93d572156397eee54e4f5aa9742d8ed
mohit <mohit.rathee2505@gmail.com>
2024-12-13 13:53:04 +0530

    covered the case of colinear lines

--- advent2024/day13/part2.py
+++ advent2024/day13/part2.py
@@ -34,34 +34,69 @@ def main(input):
         [b1x, b1y] = b1
         [b2x, b2y] = b2
         [px, py] = p
+        px += 10000000000000
+        py += 10000000000000
 
-        px+=10000000000000
-        py+=10000000000000
-        a1, b1, c1 = b1x, b2x, px  
-        a2, b2, c2 = b1y, b2y, py  
+        a1, b1, c1 = b1x, b2x, px
+        a2, b2, c2 = b1y, b2y, py
 
+        # print('A =', a1, 'x + ', b1, 'y = ', c1)
+        # print('B =', a2, 'x + ', b2, 'y = ', c2)
+        # print('prize at',(px,py))
         solution = solve_system(a1, b1, c1, a2, b2, c2)
-        [A,B] = solution
         if solution:
+            [A, B] = solution
             if is_whole(A) and is_whole(B):
-                # print(b1x,b2x)
+                # print(b1x, b2x)
                 # print(f"Solution: A = {A}, B = {B}")
                 token = A*3+B
                 # print(token)
-                tokens+=token
+                tokens += token
+        else:
+            # this edge case was never used in input
+            x_factor = a2/a1
+            y_factor = b2/b1
+            c_factor = c2/c1
+            # print('x',x_factor)
+            # print('y',y_factor)
+            # print('c',c_factor)
+            if x_factor == y_factor and x_factor == c_factor:
+                print('coolinear')
+                A = c1 // a1
+                B = c2 // b2
+                B_token = B
+                A_token = A*3
+                print('A', A)
+                print('B', B)
+                final_token = 0
+                if A <= 0:  # Consider B
+                    # print('B_token',B_token)
+                    final_token = B_token
+                elif B <= 0:  # Consider A
+                    # print('A_token',A_token)
+                    final_token = A_token
+                else:  # Compare
+                    final_token = min(A_token, B_token)
+                print('final_token', final_token)
+                tokens += final_token
+
+            elif x_factor == y_factor:
+                print('parallel', 0)
+                continue
 
     print(int(tokens))
+
 
 def is_whole(num):
     if num == int(num):
         return True
-    else: 
+    else:
         return False
+
 
 def solve_system(a1, b1, c1, a2, b2, c2):
     det = a1 * b2 - a2 * b1
     if det == 0:
-        print('error')
         return False
 
     A = (c1 * b2 - c2 * b1) / det
