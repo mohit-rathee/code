fba2cae754c0dedda94071bcb8641d7aa36c1213
mohit <mohit.rathee2505@gmail.com>
2024-12-14 12:54:02 +0530

    checking if no of components are at any each stage

--- advent2024/day14/part2.py
+++ advent2024/day14/part2.py
@@ -12,42 +12,78 @@ def parseInput(file):
 def printGrid(grid):
     for i in grid:
         for j in i:
-            if type(j) is int:
-                print('*', end=' ')
-            else:
-                print(' ', end=' ')
+            print(j, end='')
         print()
+
+
+dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
 
 def main(inpu):
     width = 101
     height = 103
+    # width = 11
+    # height = 7
     step = 1
 
     data = parseInput(inpu)
-    for i in range(1,10**4):
-        # s = input(':')
-        print(i)
+    for i in range(1, 10**4):
+        # if i % 1000 == 0:
+        #     print(i)
         grid = [['.']*width for i in range(height)]
-        for i in range(len(data)):
-            p,v = data[i]
+        for j in range(len(data)):
+            p, v = data[j]
             px, py = p
             vx, vy = v
-            # print('adding', (vx, vy))
             px += (vx*step)
             py += (vy*step)
-            # print('newPos', (px, py))
             px = px % width
             py = py % height
-            data[i][0][0]= px
-            data[i][0][1]= py
-            # print('newPos', (px, py))
+            data[j][0][0] = px
+            data[j][0][1] = py
             if px in range(width) and py in range(height):
-                if grid[py][px] == '.':
-                    grid[py][px] = 1
-                else:
-                    grid[py][px] += 1
-            # print('pos', p)
-            # print('vel', v)
-        printGrid(grid)
-        print()
+                grid[py][px] = '#'
+
+        SEEN = set()
+        components = 0
+        for x in range(height):
+            for y in range(width):
+                if (x, y) not in SEEN and grid[x][y] == "#":
+                    # print(SEEN)
+                    components += 1
+                    SEEN.add((x, y))
+                    # print('new components added', (x, y))
+                    for dir in dirs:
+                        nx = x + dir[0]
+                        ny = y + dir[1]
+                        traverse(nx, ny, grid, SEEN)
+        if components<=200:
+            print(i, components)
+            printGrid(grid)
+            print()
+            break
+
+
+def traverse(x, y, grid, SEEN):
+    # print('checking ',(x,y))
+    mx = len(grid)
+    my = len(grid[0])
+    if x not in range(mx) or y not in range(my):
+        # print('out of bound')
+        return
+
+    if (x, y) in SEEN:
+        # print('alreadey there')
+        return
+    # printGrid(grid)
+    # print(grid[y][x])
+    if grid[x][y] == "#":
+        SEEN.add((x, y))
+        # print('added',(x,y))
+        for dir in dirs:
+            nx = x + dir[0]
+            ny = y + dir[1]
+            traverse(nx, ny, grid, SEEN)
+    # else:
+    #     print('# not there')
+    return
