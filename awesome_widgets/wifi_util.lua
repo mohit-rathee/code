a4ed2ce9803b238ba4383e3446d6e40f8728475a
mohit <mohit.rathee2505@gmail.com>
2025-01-31 19:45:42 +0530

    updates

--- awesome_widgets/wifi_util.lua
+++ awesome_widgets/wifi_util.lua
@@ -1,4 +1,6 @@
 local awful = require("awful")
+local gears = require("gears")
+local beautiful = require("beautiful")
 local wibox = require("wibox")
 --local watch = require("awful.widget.watch")
 local spawn = require("awful.spawn")
@@ -17,20 +19,20 @@ local blutoth_check = "bluetoothctl info 74:D7:13:ED:10:00"
 local blutoth_ch_regex = "connected: yes"
 local blutoth_connect = "bluetoothctl connect 74:D7:13:ED:10:00"
 
-local function run_cmd (device,connect_cmd,conn_regex)
-      --spawn.easy_async(CMD,callback(stdout, stderr, reason, exit_code))
-        naughty.notify({text="trying to connect"})
-        spawn.easy_async(connect_cmd,function(conn_out)
-            conn_regex = string.format('%s',conn_regex,'%s')
-            local connected = string.match(conn_out:lower(),conn_regex)
-            if connected then
-                naughty.notify({ text = "connected with device"})
-            else
-                naughty.notify({ text = "can't find device"})
-            end
-        end)
+local function run_cmd(device, connect_cmd, conn_regex)
+    --spawn.easy_async(CMD,callback(stdout, stderr, reason, exit_code))
+    naughty.notify({ text = "trying to connect" })
+    spawn.easy_async(connect_cmd, function(conn_out)
+        conn_regex = string.format('%s', conn_regex, '%s')
+        local connected = string.match(conn_out:lower(), conn_regex)
+        if connected then
+            naughty.notify({ text = "connected with device" })
+        else
+            naughty.notify({ text = "can't find device" })
+        end
+    end)
 end
-local function worker (user_args)
+local function worker(user_args)
     local args = user_args or {}
     local text
     if args.mode == "wifi" or args.mode == "bluetooth" then
@@ -40,6 +42,23 @@ local function worker (user_args)
     end
     wifi_util.widget = wibox.widget.textbox()
     wifi_util.widget.text = text
+
+    local function rounded_shape(size)
+        return function(cr, width, height)
+            gears.shape.rounded_rect(cr, width, height, size)
+        end
+    end
+
+    local cal = wibox.widget {}
+
+    local popup = awful.popup {
+        ontop = true,
+        visible = true,
+        shape = rounded_shape(8),
+        offset = { y = 5 },
+        border_width = 1,
+        widget = cal
+    }
 
 
     function wifi_util:toggle()
@@ -51,8 +70,9 @@ local function worker (user_args)
         end
         wifi_util:check(true)
     end
+
     --wifi_util:check("wifi",wifi_check,"connected")
-    function wifi_util:check(show_notification,callback)
+    function wifi_util:check(show_notification, callback)
         local ch_cmd
         local ch_rgx
         local device = wifi_util.widget.text
@@ -63,55 +83,57 @@ local function worker (user_args)
             ch_cmd = blutoth_check
             ch_rgx = blutoth_ch_regex
         end
---      local watcher = watch("ls", 2, function ()
---        naughty.notify({text="hd"})
---      end,wifi_util)
+        --      local watcher = watch("ls", 2, function ()
+        --        naughty.notify({text="hd"})
+        --      end,wifi_util)
         awful.spawn.easy_async(ch_cmd,
-                function(stdout, _, _, _)
-                    if notification then
-                        naughty.destroy(notification)
-                        notification = false
+            function(stdout, _, _, _)
+                if notification then
+                    naughty.destroy(notification)
+                    notification = false
+                end
+                ch_rgx = string.format('%%s%s%%s', ch_rgx)
+                local connected = string.match(stdout:lower(), ch_rgx)
+                local result
+                if connected then
+                    result = "connected"
+                    if not show_notification then
+                        callback(device, true)
                     end
-                    ch_rgx = string.format('%%s%s%%s',ch_rgx)
-                    local connected = string.match(stdout:lower(),ch_rgx)
-                    local result
-                    if connected then
-                        result = "connected"
-                        if not show_notification then
-                            callback(device,true)
-                        end
-                    else
-                        result = "not connected"
-                        if not show_notification then
-                            callback(device,false)
-                        end
+                else
+                    result = "not connected"
+                    if not show_notification then
+                        callback(device, false)
                     end
-                    notification = naughty.notify {
-                        title = wifi_util.widget.text ,
-                        text = result,
-                        width = 200,
-                        --timeout = 3,
-                    }
-                end)
+                end
+                notification = naughty.notify {
+                    title = wifi_util.widget.text,
+                    text = result,
+                    width = 200,
+                    --timeout = 3,
+                }
+            end)
     end
+
     function wifi_util:connect()
-        wifi_util:check(false,function (device,is_conn)
+        wifi_util:check(false, function(device, is_conn)
             if not is_conn then
                 if device == "wifi" then
-                    run_cmd(device,wifi_connect,"successfully")
+                    run_cmd(device, wifi_connect, "successfully")
                 else
-                    run_cmd(device,blutoth_connect,"successful")
+                    run_cmd(device, blutoth_connect, "successful")
                 end
             end
         end)
     end
+
     wifi_util.widget:buttons(
-            awful.util.table.join(
-                    awful.button({}, 1, function() wifi_util:connect() end),
-                    awful.button({}, 3, function() wifi_util:toggle() end)
-                    --awful.button({}, 4, function() wifi_util:check_wifi() end)
-                    --awful.button({}, 5, function() wifi_util:toggle() end)
-            )
+        awful.util.table.join(
+            awful.button({}, 1, function() wifi_util:connect() end),
+            awful.button({}, 3, function() wifi_util:toggle() end)
+        --awful.button({}, 4, function() wifi_util:check_wifi() end)
+        --awful.button({}, 5, function() wifi_util:toggle() end)
+        )
     )
 
     wifi_util.widget:connect_signal("mouse::enter", function() wifi_util:check(true) end)
@@ -120,6 +142,8 @@ local function worker (user_args)
 end
 
 
-return setmetatable(wifi_util, { __call = function(_, ...)
-    return worker(...)
-end })
+return setmetatable(wifi_util, {
+    __call = function(_, ...)
+        return worker(...)
+    end
+})
--- gfg/incrementalgroup(8points).py
+++ gfg/incrementalgroup(8points).py
@@ -12,7 +12,7 @@
 #         # print("found")
 #         # print(str(newarr))
 #         # print(ans)
-#         return  
+#         return
 #     ans.append(str(newarr))
 #     global length
 #     length+=1
@@ -53,19 +53,18 @@
 #     # print(split)
 
 
-
 #     if split:
 #         return split
 #     return 0
 
-# def solve(arr,K):   
+# def solve(arr,K):
 #     if arr ==None:
 #         return
 #     split=splitTree(arr, K)
 #     # print(arr)
 #     if split ==0:
 #         # print("reached end")
-#         return 
+#         return
 #     if len(split)==1:
 #         # print("split at :")
 #         # print(split)
@@ -82,7 +81,6 @@
 #     return 0
 
 
-        
 # def countWaystoDivide(N, K):
 #     arr=[]
 #     for i in range (K-1):
@@ -97,41 +95,39 @@
 #     print(len(ans))
 
 
-
 # countWaystoDivide(60,43)
 
+# Function to count the number
+# of ways to divide the number N
+# in groups such that each group
+# has K number of elements
+def calculate(self, pos, prev, left, K, dp):
 
- # Function to count the number
-    # of ways to divide the number N
-    # in groups such that each group
-    # has K number of elements
-    def calculate(self, pos, prev, left, K, dp):
-        
-        # Base Case
-        if (pos == K):
-            if (left == 0):
-                return 1
-            else:
-                return 0
-    
-        # If N is divides completely
-        # into less than K groups
+    # Base Case
+    if (pos == K):
         if (left == 0):
+            return 1
+        else:
             return 0
-        if(dp[pos][left][prev]!=-1):
-            return dp[pos][left][prev]
-        answer = 0
-    
-        # Put all possible values
-        # greater equal to prev
-        for i in range(prev, left + 1):
-            answer += self.calculate(pos + 1, i, left - i, K,dp)
-           
-        dp[pos][left][prev]= answer
-        return answer
-        
-    def countWaystoDivide(self, N, K):
-        dp = [[[-1 for j in range(N+1)] for j in range(N+1)] for j in range(K+1)]
-        return self.calculate(0, 1, N, K, dp)
-     
-    
+
+    # If N is divides completely
+    # into less than K groups
+    if (left == 0):
+        return 0
+    if (dp[pos][left][prev] != -1):
+        return dp[pos][left][prev]
+    answer = 0
+
+    # Put all possible values
+    # greater equal to prev
+    for i in range(prev, left + 1):
+        answer += self.calculate(pos + 1, i, left - i, K, dp)
+
+    dp[pos][left][prev] = answer
+    print(answer)
+    return answer
+
+
+def countWaystoDivide(self, N, K):
+    dp = [[[-1 for j in range(N+1)] for j in range(N+1)] for j in range(K+1)]
+    return self.calculate(0, 1, N, K, dp)
