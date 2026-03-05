ed0e48559fa5a261342274721b08521ed91ca793
mohit-rathee <mohit.rathee2505@gmail.com>
2026-03-05 16:49:28 +0530

    notify_lib init

---
+++ awesome_widgets/notify_lib.lua
@@ -0,0 +1,30 @@
+local naughty = require("naughty")
+local gears = require("gears")
+local wibox = require("wibox")
+
+naughty.connect_signal("request::display", function(n)
+    n.timeout = 2
+    naughty.layout.box({
+        notification = n,
+        border_width = 0,
+        shape = gears.shape.rounded_rect,
+
+        widget_template = {
+            {
+                {
+                    naughty.widget.icon,
+                    naughty.widget.title,
+                    naughty.widget.message,
+                    spacing = 8,
+                    layout = wibox.layout.fixed.vertical,
+                },
+                margins = 8,
+                widget = wibox.container.margin
+            },
+
+            bg = "#1e1e2e",
+            shape = gears.shape.rounded_rect,
+            widget = wibox.container.background
+        }
+    })
+end)
