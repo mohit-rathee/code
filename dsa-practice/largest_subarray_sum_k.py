a844819fce228f1d50ae42481c2f9af34aae3391
mohit-rathee <mohit.rathee2505@gmail.com>
2026-06-08 20:26:14 +0530

    largest subarray with sum k(prefix hashmap)

--- dsa-practice/largest_subarray_sum_k.py
+++ dsa-practice/largest_subarray_sum_k.py
@@ -2,23 +2,25 @@ def largest_subarray_with_sum_k(nums, k):
     subarray = [-1, -1]
     count = 0
     sum = 0
-    prefix_sum = [0 for i in range(len(nums))]
+    prefix_sum = {}
     for i in range(len(nums)):
         num = nums[i]
         sum += num
+        # i would always be bigger!
+        # prefix_sum[sum] = min(prefix_sum[sum], i)
         if sum == k:
             # len of subarray is 1
-            count = max(count, 1)
-            subarray[0] = i
-            subarray[1] = i
+            if 1 > count:
+                count = 1
+                subarray[0] = subarray[1] = i
         else:
-            for j in range(i):
-                if sum - prefix_sum[j] == k:
-                    count = max(count, i-j)
-                    subarray[0] = j
-                    subarray[1] = i
-                    break
-        prefix_sum[i] = sum
+            req_no = sum - k
+            if req_no in prefix_sum:
+                count = max(count, i - prefix_sum[req_no])
+                subarray[0] = prefix_sum[req_no]
+                subarray[1] = i
+        if sum not in prefix_sum:
+            prefix_sum[sum] = i
 
     print(nums)
     print(prefix_sum)
